f56f0a02625a3b64be82c779e46a05f3285492d8
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-14 12:13:28 -0400

    commit tree 3eaf07cf481c9a0f0d8bda188d6a0a81525ac7f7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
   
   def index
+    byebug
     session[:session_hello] ||= "World"
+    session[:session_yo] ||= "Yo"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
   end
